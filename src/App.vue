8b84da439229ab825ed31e1bf146bca4797cb99b
吴小星 <2411147266@qq.com>
2025-03-30 01:00:23 +0800

    feat: block-time-picker 组件

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,120 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import WebTimePicker from '@/components/WebTimePicker.vue'
+import { ref, reactive } from 'vue'
+import { pick } from 'lodash-es'
+import { QuestionFilled } from '@element-plus/icons-vue'
+
+const defaultTimeRange = ref(['09:00', '18:00'])
+const timeRange1 = ref([])
+const timeRange2 = ref([])
+const timeRange3 = ref([])
+const timeRange4 = ref([])
+
+const timeRange60 = ref(['09:00', '23:59'])
+const timeRange30 = ref(['09:30', '18:30'])
+const timeRange20 = ref(['09:40', '16:00'])
+const timeRange15 = ref(['09:45', '13:30'])
+const timeRange10 = ref(['09:50', '12:30'])
+
+const timeStepObj = {
+  10: '10分',
+  15: '15分',
+  20: '20分',
+  30: '30分',
+  60: '60分',
+}
+const options = reactive({
+  minHour: '0',
+  maxHour: '23',
+  step: 30,
+  activeColor: '#FFCD5E',
+  readonly: true,
+  disabledBefore: true,
+})
+
+const valueMap = new Map([
+  [10, timeRange10],
+  [15, timeRange15],
+  [20, timeRange20],
+  [30, timeRange30],
+  [60, timeRange60],
+])
+
+const getTimeStr = (times) => {
+  return times.length ? times.join(' ~ ') : '-'
+}
+
+function handleValue(step) {
+  return valueMap.get(+step).value
+}
+
+const timePickerChange = (val, step) => {
+  valueMap.get(+step).value = val
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <el-card shadow="never">
+    <template #header>
+      <el-row type="flex" align="middle" justify="space-between">
+        <div class="flex-c">
+          块状时间段选择组件(自主封装)&nbsp;
+          <el-tooltip trigger="click">
+            <template #content>
+              <p>
+                操作交互逻辑：<br />
+                1.当选择了某一节，点击该节之前则是选择开始时间，点击该节之后则是选择结束时间；<br />
+                2.开始和结束时间均选择完毕，选择高亮区域之前则重置，选择高亮区域之后则是重新选择结束时间；<br />
+                3.开始和结束时间均选择完毕，往前选择则重置，往后选择则是选择结束时间；<br />
+                3.开始和结束时间均选择完毕，点击高亮区域中间（不含第一节或最后一节），则是重新选择结束时间；<br />
+                4.开始和结束时间均选择完毕，点击高亮区域第一节或最后一节均是取消选择该时间。<br />
+              </p>
+            </template>
+            <el-icon class="cursor-pointer"><QuestionFilled /></el-icon>
+          </el-tooltip>
+        </div>
+      </el-row>
+    </template>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+      <el-divider content-position="left">
+        基础使用: 已选时间 {{ getTimeStr(timeRange1) }}</el-divider
+      >
+      <WebTimePicker v-model="timeRange1" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <el-divider content-position="left">
+        默认选中时间: 已选时间 {{ getTimeStr(defaultTimeRange) }}</el-divider
+      >
+      <WebTimePicker v-model="defaultTimeRange" />
+
+      <el-divider content-position="left">
+        自定义选中高亮颜色（activeColor） 已选时间 {{ getTimeStr(timeRange2) }}</el-divider
+      >
+      <WebTimePicker v-model="timeRange2" v-bind="pick(options, ['activeColor'])" />
+
+      <el-divider content-position="left">
+        指定可选范围（minHour：9，maxHour: 18） 已选时间
+        {{ getTimeStr(timeRange3) }}</el-divider
+      >
+      <WebTimePicker v-model="timeRange3" :min-hour="9" :max-hour="18" />
+
+      <el-divider content-position="left">
+        只读（readonly）和禁用过去时间（disabledBefore） 已选时间
+        {{ getTimeStr(timeRange4) }}
+      </el-divider>
+      <WebTimePicker v-model="timeRange4" v-bind="pick(options, ['readonly', 'disabledBefore'])" />
+
+      <el-divider content-position="left">自定义时间间隔（step: 60|30|20|15|10）</el-divider>
+      <template v-for="step of Object.keys(timeStepObj).reverse()" :key="step">
+        <p class="my-2">每隔 {{ step }} 分钟 已选时间：{{ getTimeStr(handleValue(step)) }}</p>
+        <WebTimePicker
+          :model-value="handleValue(step)"
+          :step="step"
+          @change="timePickerChange($event, step)"
+        />
+      </template>
     </div>
-  </header>
-
-  <RouterView />
+  </el-card>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/MobieTimePicker.vue
@@ -0,0 +1,452 @@
+<!--
+ * @Description:块状选择时间 - 适用于移动端
+ * @Author: wuxxing
+-->
+<template>
+  <div class="block-time-picker">
+    <div class="time-wrapper">
+      <template v-for="(bar, index) in timeData" :key="bar.time + index">
+        <div class="time-bar">
+          <span class="time-text">{{ bar.time + ':00' }}</span>
+          <div class="time-full">
+            <div
+              v-for="block in bar.blocks"
+              :key="block.value"
+              :data-val="block.value"
+              :data-idx="block.index"
+              class="time-block"
+              :class="{
+                'time-block--selected':
+                  timeIdxRange.includes(block.index) || moveIndex == block.index,
+                'time-block--readonly': readonly,
+                'time-block--disabled': block.disabled,
+                'in-range':
+                  inRange.length == 2 && block.index > inRange[0] && block.index < inRange[1],
+              }"
+              @click="onClickBlock(bar, block, block.index)"
+              @mousemove="onmousemove(block.index)"
+              @mouseleave="onmouseleave(block.index)"
+            />
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, nextTick, computed, toRefs } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    default: () => [],
+  },
+  // 最小小时数
+  minHour: {
+    type: [Number, String],
+    default: '0',
+  },
+  // 最大小时数
+  maxHour: {
+    type: [Number, String],
+    default: '23',
+  },
+  // 时间间隔：必须是被 60 分钟整除
+  step: {
+    type: [Number, String],
+    default: 30,
+    validator: (val) => {
+      return 60 % parseInt(val) === 0
+    },
+  },
+  // 选择范围时的分隔符
+  rangeSeparator: {
+    type: String,
+    default: ' ~ ',
+  },
+  // 激活高亮颜色
+  activeColor: {
+    type: String,
+    default: '#2e6cf3',
+  },
+  // 未激活颜色
+  inactiveColor: {
+    type: String,
+    default: '#a0a6ae',
+  },
+  readonly: Boolean, // 是否只读
+  disabledBefore: Boolean, // 是否禁用过去的时间
+
+  // TODO: 尺寸
+  size: {
+    type: String,
+    default: 'default',
+    validatior(val) {
+      return ['large', 'default', 'small'].includes(val)
+    },
+  },
+})
+
+const emit = defineEmits(['update:modelValue', 'change'])
+
+const barWidth = ref(0)
+
+const timeData = ref([]) // 渲染数据
+const timeIdxRange = ref([]) //选择开始和结束时间的索引
+const startIndex = ref()
+const endIndex = ref()
+
+const moveIndex = ref() // 记录当前鼠标移入索引
+const inRange = computed(() => {
+  let range = []
+  if (startIndex.value !== undefined && moveIndex.value !== undefined) {
+    range = [startIndex.value, moveIndex.value].sort((a, b) => a - b)
+  }
+  return range
+}) // 记录开始索引和鼠标
+
+onMounted(() => {
+  initTimeData()
+  initTimeRange()
+  getBarWidth()
+})
+
+const getBarWidth = async () => {
+  await nextTick()
+  barWidth.value = document.querySelector('.time-bar')?.getBoundingClientRect()?.width
+}
+
+// 分钟转为整点小时
+const minute2hour = (minute) => {
+  return String(Math.floor(minute / 60)).padStart(2, '0')
+}
+
+// 更新值
+const updateValue = () => {
+  let value = []
+  if (timeIdxRange.value.length === 0) {
+    value = []
+    emit('update:modelValue', value)
+    emit('change', value)
+    return
+  }
+  const startTime = +props.minHour * 60 + Number(props.step) * startIndex.value + Number(props.step)
+  const endTime = +props.minHour * 60 + Number(props.step) * endIndex.value + Number(props.step)
+  let eHour = undefined // 结束小时
+  let eMin = undefined // 结束分钟
+  let sHour = undefined // 开始小时
+  let sMin = undefined // 开始分钟
+  let sTime = undefined // 开始时间
+  let eTime = undefined // 结束时间
+  if (startTime && endTime) {
+    sHour = minute2hour(startTime - +props.step)
+    eHour = minute2hour(endTime)
+    sMin =
+      (startTime - +props.step) % 60 === 0
+        ? '00'
+        : Math.round(((startTime - +props.step) / 60 - sHour) * 60)
+    eMin = endTime % 60 === 0 ? '00' : Math.round((endTime / 60 - eHour) * 60)
+    sTime = sHour + ':' + sMin
+    // 处理临界时间
+    if (+eHour === 24) {
+      eHour = 23
+      eMin = 59
+    }
+    eTime = eHour + ':' + eMin
+  } else {
+    sHour = minute2hour(startTime - +props.step)
+    eHour = minute2hour(startTime)
+    sMin =
+      (startTime - +props.step) % 60 === 0
+        ? '00'
+        : Math.round(((startTime - +props.step) / 60 - sHour) * 60)
+    eMin = startTime % 60 === 0 ? '00' : Math.round((startTime / 60 - eHour) * 60)
+
+    sTime = sHour + ':' + sMin
+    // 处理临界时间
+    if (+eHour === 24) {
+      eHour = 23
+      eMin = 59
+    }
+    eTime = eHour + ':' + eMin
+  }
+
+  value = [sTime, eTime]
+  emit('update:modelValue', value)
+  emit('change', value)
+}
+
+// 初始化时间范围
+const initTimeData = () => {
+  const nowHour = new Date().getHours()
+  const nowMin = new Date().getMinutes()
+
+  const times = []
+  const blocks = (time) => {
+    const temp = []
+    const idx = (60 / +props.step) * (time - +props.minHour)
+
+    const handleDisabled = (time, i) => {
+      if (!props.disabledBefore) {
+        return false
+      }
+      if (time < nowHour) {
+        return true
+      }
+      if (nowHour === time) {
+        const s = +props.step * (i - 1)
+        const e = +props.step * i
+        if (nowMin >= s && nowMin <= e) {
+          return true
+        }
+        if (nowMin >= e) {
+          return true
+        }
+      }
+    }
+    for (let i = 1; i <= 60 / +props.step; i++) {
+      temp.push({
+        value: time * 60 + props.step * i,
+        selected: false,
+        disabled: handleDisabled(time, i),
+        index: idx + i - 1,
+      })
+    }
+    return temp
+  }
+
+  for (let i = +props.minHour; i <= +props.maxHour; i++) {
+    const item = {
+      time: undefined,
+      blocks: blocks(i),
+    }
+    item.time = minute2hour(i * 60)
+    times.push(item)
+  }
+  timeData.value = times
+}
+
+// 回显
+const initTimeRange = () => {
+  const { modelValue, step } = toRefs(props)
+  if (Array.isArray(modelValue.value)) {
+    if (modelValue.value.length === 2) {
+      const getHourByTime = (time) => {
+        return time.split(':')[0].padStart(2, '0')
+      }
+      const getMinByTime = (time) => {
+        return time.split(':')[1].padStart(2, '0')
+      }
+      let startIdx = 0
+      let endIdx = 0
+      const [start, end] = modelValue.value
+
+      startIdx = timeData.value.findIndex((item) => item.time === getHourByTime(start))
+      endIdx = timeData.value.findIndex((item) => item.time === getHourByTime(end))
+
+      startIndex.value = startIdx * (60 / step.value)
+      endIndex.value = endIdx * (60 / step.value)
+
+      if (parseInt(getMinByTime(start)) == step.value) {
+        const i = getMinByTime(start) / step.value
+        startIndex.value = startIndex.value + (i > 0 ? i : 0)
+      } else {
+        const i = getMinByTime(start) / step.value
+        console.log('i', i)
+        startIndex.value = startIndex.value + (i > 0 ? i : 0)
+      }
+
+      if (parseInt(getMinByTime(end)) === 0) {
+        endIndex.value = endIndex.value - 1
+      } else if (parseInt(getMinByTime(end)) === 59) {
+        const i = 60 / step.value
+        console.log('i', i)
+        endIndex.value = endIndex.value - 1 + (i > 0 ? i : 0)
+      } else {
+        const i = getMinByTime(end) / step.value
+        console.log('i', i)
+        endIndex.value = endIndex.value - 1 + (i > 0 ? i : 0)
+      }
+
+      console.log(startIdx, endIdx, startIndex.value, endIndex.value, start, end)
+      for (let i = startIndex.value; i <= endIndex.value; i++) {
+        timeIdxRange.value.splice(i - startIndex.value, 0, i)
+      }
+    }
+  } else {
+    throw new Error(`modelValue must be a array, and the maxlength should be zero or two.`)
+  }
+}
+
+// 点击选择
+const onClickBlock = (bar, block, index) => {
+  if (props.readonly) {
+    return false
+  }
+  if (props.disabledBefore) {
+    initTimeData()
+  }
+  if (block.disabled) {
+    return
+  }
+  moveIndex.value = undefined
+
+  if (startIndex.value === undefined && endIndex.value === undefined) {
+    startIndex.value = timeIdxRange.value[0] = index
+  }
+  // 当只存在开始时间时
+  else if (startIndex.value !== undefined && endIndex.value === undefined) {
+    if (index === startIndex.value) {
+      // 双击取反
+      if (timeIdxRange.value.indexOf(index) > -1) {
+        timeIdxRange.value.splice(timeIdxRange.value.indexOf(index), 1)
+        startIndex.value = endIndex.value = undefined
+      } else {
+        // timeIdxRange.value.push(startIndex.value)
+      }
+    }
+    if (index > startIndex.value) {
+      endIndex.value = index
+      timeIdxRange.value.splice(0)
+      for (let i = startIndex.value; i <= endIndex.value; i++) {
+        timeIdxRange.value.splice(i - startIndex.value, 0, i)
+      }
+    }
+    if (index < startIndex.value) {
+      const [start, end] = [index, startIndex.value]
+      startIndex.value = start
+      endIndex.value = end
+      // 选取数据--向右添加，向左取消
+      timeIdxRange.value.splice(0)
+      for (let i = startIndex.value; i <= endIndex.value; i++) {
+        timeIdxRange.value.splice(i - startIndex.value, 0, i)
+      }
+    }
+  }
+  // 同时存在开始结束时间，向左则重置选择
+  else if (startIndex.value !== undefined && endIndex.value !== undefined) {
+    if (index === startIndex.value) {
+      const idx = timeIdxRange.value.indexOf(index)
+      timeIdxRange.value.splice(idx, 1)
+      if (timeIdxRange.value.length) {
+        startIndex.value = timeIdxRange.value[0]
+      } else {
+        startIndex.value = endIndex.value = undefined
+      }
+    } else if (index === endIndex.value) {
+      const idx = timeIdxRange.value.indexOf(index)
+      timeIdxRange.value.splice(idx, 1)
+      endIndex.value = timeIdxRange.value[timeIdxRange.value.length - 1]
+      if (startIndex.value === endIndex.value) {
+        endIndex.value = undefined
+      }
+    } else if (index < startIndex.value) {
+      timeIdxRange.value.splice(0)
+      startIndex.value = endIndex.value = undefined
+    } else if (index > startIndex.value) {
+      endIndex.value = index
+      timeIdxRange.value.splice(0)
+      for (let i = startIndex.value; i <= endIndex.value; i++) {
+        timeIdxRange.value.splice(i - startIndex.value, 0, i)
+      }
+    }
+  } else {
+    //
+  }
+
+  updateValue()
+}
+
+// 鼠标移入
+const onmousemove = (index) => {
+  if (props.readonly) {
+    return
+  } else if (timeIdxRange.value.length > 1 || startIndex.value === undefined) {
+    return
+  }
+  moveIndex.value = index
+}
+
+// 鼠标移出
+const onmouseleave = () => {
+  if (props.readonly) {
+    return
+  } else if (timeIdxRange.value.length > 1 || startIndex.value === undefined) {
+    return
+  }
+  moveIndex.value = undefined
+}
+</script>
+
+<style lang="scss" scoped>
+.block-time-picker {
+  overflow: hidden;
+  display: flex;
+  box-sizing: border-box;
+  position: relative;
+
+  .time-wrapper {
+    display: flex;
+    overflow-x: auto;
+  }
+
+  .time-bar {
+    &:not(:last-child) {
+      padding-right: 8px;
+    }
+
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .time-text {
+      font-size: 12px;
+      margin-bottom: 5px;
+    }
+  }
+
+  .time-full {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+  }
+
+  .time-block {
+    width: 32px;
+    height: 40px;
+    border-radius: 2px;
+    line-height: 40px;
+    text-align: center;
+    cursor: pointer;
+    background-color: v-bind(inactiveColor);
+    transition: all 0.2s ease-in;
+    will-change: auto;
+
+    &:not(:last-child) {
+      margin-bottom: 3px;
+    }
+
+    &--selected {
+      background-color: v-bind(activeColor);
+    }
+    &--readonly {
+      opacity: 0.5;
+      cursor: default;
+    }
+    &--disabled {
+      cursor: not-allowed;
+      background: repeating-linear-gradient(-45deg, #cccccc, #ebebeb 4px);
+    }
+  }
+
+  .in-range {
+    background-color: v-bind(activeColor);
+    opacity: 0.35;
+  }
+}
+</style>
